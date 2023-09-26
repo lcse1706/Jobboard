29587ff496d83a789a5763b4c52edb2375d8156e
lukczar1706 <lcse.2001@gmail.com>
2023-09-26 22:08:51 +0200

    CSS

--- components/ui/Navigation/Navigation.tsx
+++ components/ui/Navigation/Navigation.tsx
@@ -8,7 +8,7 @@ interface NavProps {
 export const Navigation = (props: NavProps) => {
   return (
     <nav className="flex w-2/5 justify-end items-end">
-      <ul className="flex flex-row grow">
+      <ul className="flex flex-row space-x-2">
         {props.nav.map((item, index) => (
           <li key={index} className="grow text-center py-2 px-4 m-1">
             <Link href={item.link}>{item.label}</Link>
