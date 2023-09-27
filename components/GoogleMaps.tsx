15293cc026b5ff1bb18f5a1680e03e215446ac6e
lukczar1706 <lcse.2001@gmail.com>
2023-09-27 21:53:16 +0200

    Markers Added

--- components/GoogleMaps.tsx
+++ components/GoogleMaps.tsx
@@ -1,33 +1,32 @@
 'use client';
 
-import { useLoadScript, GoogleMap } from '@react-google-maps/api';
+import { useDataContext } from '@/context/Data.Context';
+import { useLoadScript, GoogleMap, MarkerF } from '@react-google-maps/api';
 import type { NextPage } from 'next';
 import { useMemo } from 'react';
 
 const mapStyles = {
   width: '100%',
-  height: '50%',
+  height: '100%',
 };
 
 export const GoogleMaps: NextPage = () => {
-  const libraries = useMemo(() => ['places'], []);
-  const mapCenter = useMemo(
-    () => ({ lat: 27.672932021393862, lng: 85.31184012689732 }),
-    []
-  );
+  // const libraries = useMemo(() => ['places'], []);
+  const mapCenter = useMemo(() => ({ lat: 51.8689731, lng: 19.2029511 }), []);
+  const { records } = useDataContext();
 
   const mapOptions = useMemo<google.maps.MapOptions>(
     () => ({
       disableDefaultUI: true,
       clickableIcons: true,
-      scrollwheel: false,
+      scrollwheel: true,
     }),
     []
   );
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOGGLE_API as string,
-    libraries: libraries as any,
+    // libraries: libraries as any,
   });
 
   if (!isLoaded) {
@@ -37,11 +36,19 @@ export const GoogleMaps: NextPage = () => {
   return (
     <GoogleMap
       options={mapOptions}
-      zoom={14}
+      zoom={6}
       center={mapCenter}
       mapTypeId={google.maps.MapTypeId.ROADMAP}
       mapContainerStyle={mapStyles}
       onLoad={() => console.log('Map Component Loaded...')}
-    />
+    >
+      {records.map((pos, index) => (
+        <MarkerF
+          key={index}
+          position={pos.coordinates}
+          onClick={() => console.log('Marker Added')}
+        />
+      ))}
+    </GoogleMap>
   );
 };
