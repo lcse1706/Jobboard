9c00e51327743d8550c641c5a35069a4cd9e5cf6
lukczar1706 <lcse.2001@gmail.com>
2023-09-27 21:52:26 +0200

    css

--- app/JobOfferMap.tsx
+++ app/JobOfferMap.tsx
@@ -2,7 +2,7 @@ import { GoogleMaps } from '@/components/GoogleMaps';
 
 export const JobOfferMap = () => {
   return (
-    <section className="w-1/3">
+    <section className="w-1/3 rounded-md border border-gray-300">
       <GoogleMaps />
     </section>
   );
