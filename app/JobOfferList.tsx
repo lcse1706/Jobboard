5e480605443d5be84f4997a2b2fa616c35571eff
lukczar1706 <lcse.2001@gmail.com>
2023-09-28 19:24:50 +0200

    data context implemented

--- app/JobOfferList.tsx
+++ app/JobOfferList.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { OfferListForm } from '@/components/OfferList.Form';
+import { useDataContext } from '@/context/Data.Context';
 import { useEffect, useState } from 'react';
 
 interface OffersT {
@@ -12,46 +13,17 @@ interface OffersT {
   description: string;
 }
 
-const JobOffers: OffersT[] = [
-  {
-    imgSrc: '/favicon.ico',
-    title: 'JS Developer',
-    salary: '20k-30k',
-    technologies: ['JS', 'CSS', 'React', 'StoryBook'],
-    localization: 'Gdansk',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate dolore ad iste expedita eaque natus excepturi beatae aut, maxime dolor!',
-  },
-  {
-    imgSrc: '/favicon.ico',
-    title: 'Python Developer',
-    salary: '25k-35k',
-    technologies: ['Python', 'Django', 'SQL', 'REST API'],
-    localization: 'Warsaw',
-    description:
-      'Poszukujemy doświadczonego programisty Python, który będzie odpowiedzialny za rozwijanie aplikacji webowych w oparciu o Django oraz tworzenie i zarządzanie bazami danych SQL.',
-  },
-  {
-    imgSrc: '/favicon.ico',
-    title: 'Frontend Developer',
-    salary: '22k-32k',
-    technologies: ['HTML', 'CSS', 'JavaScript', 'Vue.js'],
-    localization: 'Krakow',
-    description:
-      'Jesteśmy zespołem Frontend Developerów poszukującym kreatywnego i ambitnego programisty, który będzie odpowiedzialny za tworzenie atrakcyjnych i responsywnych interfejsów użytkownika.',
-  },
-];
-
 export const JobOfferList = () => {
+  const { records, setRecords } = useDataContext();
   const [searchField, setSearchField] = useState('');
-  const [filteredData, setFilteredData] = useState(JobOffers);
+  const [filteredData, setFilteredData] = useState(records);
 
   const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchField(e.target.value);
   };
 
   useEffect(() => {
-    const filteredData = JobOffers.filter(data => {
+    const filteredData = records.filter(data => {
       return (
         data.title.toLowerCase().includes(searchField.toLowerCase()) ||
         data.salary.toLowerCase().includes(searchField.toLowerCase()) ||
