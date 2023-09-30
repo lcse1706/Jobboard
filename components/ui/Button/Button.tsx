e4c695624ba750a4aa42377ea166765dca3db3cd
lukczar1706 <lcse.2001@gmail.com>
2023-10-01 00:15:40 +0200

    cleanup

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -2,29 +2,28 @@ import { ComponentProps } from 'react';
 
 interface ButtonProps {
   label: string;
+  type: string;
   onClick?: () => void;
   className?: string;
   disabled?: boolean;
 }
 
-const BASE_BUTTON_CLASSES = 'text-white font-bold py-2 px-4 rounded-full m-1';
-
-const ENABLED_BUTTON = 'bg-pink-500 hover:bg-pink-700';
-const DISABLED_BUTTON = 'bg-gray-500';
+const BASE_BUTTON_CLASSES =
+  'bg-pink-500 hover:bg-pink-700 disabled:bg-gray-500 text-white font-bold py-2 px-4 rounded-full m-1';
 
 export const Button = ({
   label,
+  type,
   className,
   disabled,
   ...rest
 }: ComponentProps<'button'> & ButtonProps) => {
+  console.log('Button Fired');
   return (
     <button
-      type="submit"
+      type={type}
       disabled={disabled}
-      className={`${className} ${BASE_BUTTON_CLASSES} ${
-        disabled ? DISABLED_BUTTON : ENABLED_BUTTON
-      }`}
+      className={`${className} ${BASE_BUTTON_CLASSES}`}
       {...rest}
     >
       {label}
