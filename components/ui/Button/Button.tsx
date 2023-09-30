a4979565b3631e41a661c26d68e8f19919571e0e
lukczar1706 <lcse.2001@gmail.com>
2023-09-30 17:21:11 +0200

    disabled button added

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -4,22 +4,28 @@ interface ButtonProps {
   label: string;
   onClick?: () => void;
   className?: string;
+  disabled?: boolean;
 }
 
-// const BASE_BUTTON_CLASSES =
-//   'bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full m-1';
-const BASE_BUTTON_CLASSES =
-  'bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full m-1';
+const BASE_BUTTON_CLASSES = 'text-white font-bold py-2 px-4 rounded-full m-1';
+
+const ENABLED_BUTTON = 'bg-pink-500 hover:bg-pink-700';
+const DISABLED_BUTTON = 'bg-gray-500';
 
 export const Button = ({
   label,
   className,
+  disabled,
   ...rest
 }: ComponentProps<'button'> & ButtonProps) => {
+  console.log({ disabled });
   return (
     <button
-      type="button"
-      className={`${className} ${BASE_BUTTON_CLASSES}`}
+      type="submit"
+      disabled={disabled}
+      className={`${className} ${BASE_BUTTON_CLASSES} ${
+        disabled ? DISABLED_BUTTON : ENABLED_BUTTON
+      }`}
       {...rest}
     >
       {label}
