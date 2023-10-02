6ccc6c4b68734cb41cd6864fd51ecba20b8887d3
lukczar1706 <lcse.2001@gmail.com>
2023-10-02 21:51:09 +0200

    cleanup

--- lib/types.ts
+++ lib/types.ts
@@ -1,12 +1,9 @@
 import { z } from 'zod';
 
 export const offerDTO = z.object({
-  // imgSrc: z.string(),
   title: z.string(),
   salary: z.string(),
   technologies: z.string(),
-  // localization: z.string(),
-  // coordinates: z.object({ lat: z.number(), lng: z.number() }),
   description: z.string(),
 });
 
