77647e8449e1f555e405a8023364ff9fc42fbd61
lukczar1706 <lcse.2001@gmail.com>
2023-10-02 21:51:52 +0200

    sending coordinates and name from autoComplete

--- components/AddOfferForm.tsx
+++ components/AddOfferForm.tsx
@@ -10,11 +10,8 @@ import { useForm } from 'react-hook-form';
 import { useLoadScript } from '@react-google-maps/api';
 import type { NextPage } from 'next';
 
-import usePlacesAutocomplete, {
-  getGeocode,
-  getLatLng,
-} from 'use-places-autocomplete';
-import styles from './Home.module.css';
+import { getGeocode, getLatLng } from 'use-places-autocomplete';
+import { PlacesAutocomplete } from './PlacesAutocomplete';
 
 //TODO Reacthookform + Zod
 
@@ -35,7 +32,7 @@ export const AddOfferForm: NextPage = () => {
 
   const [lat, setLat] = useState(27.672932021393862);
   const [lng, setLng] = useState(85.31184012689732);
-
+  const [placeName, setPlaceName] = useState<string>('');
   const libraries = useMemo(() => ['places'], []);
 
   const { isLoaded } = useLoadScript({
@@ -58,7 +55,7 @@ export const AddOfferForm: NextPage = () => {
         title: data.title,
         salary: data.salary,
         technologies: data.technologies,
-        localization: data.localization,
+        localization: placeName,
         coordinates: { lat: lat, lng: lng },
         description: data.description,
       },
@@ -97,15 +94,6 @@ export const AddOfferForm: NextPage = () => {
         {errors.technologies && (
           <p className={errorStyles}>{`${errors.technologies.message}`}</p>
         )}
-        <input
-          {...register('localization')}
-          type="text"
-          placeholder="Localization"
-          className={inputStyles}
-        />
-        {errors.localization && (
-          <p className={errorStyles}>{`${errors.localization.message}`}</p>
-        )}
 
         <PlacesAutocomplete
           onAddressSelect={address => {
@@ -114,6 +102,7 @@ export const AddOfferForm: NextPage = () => {
 
               setLat(lat);
               setLng(lng);
+              setPlaceName(address);
             });
           }}
         />
@@ -131,61 +120,6 @@ export const AddOfferForm: NextPage = () => {
 
         <Button label="Add Offer" type="submit" disabled={isSubmitting} />
       </form>
-    </div>
-  );
-};
-
-const PlacesAutocomplete = ({
-  onAddressSelect,
-}: {
-  onAddressSelect?: (address: string) => void;
-}) => {
-  const {
-    ready,
-    value,
-    suggestions: { status, data },
-    setValue,
-    clearSuggestions,
-  } = usePlacesAutocomplete({
-    requestOptions: {},
-    debounce: 300,
-    cache: 86400,
-  });
-
-  const renderSuggestions = () => {
-    return data.map(suggestion => {
-      const {
-        place_id,
-        structured_formatting: { main_text, secondary_text },
-        description,
-      } = suggestion;
-
-      return (
-        <li
-          key={place_id}
-          onClick={() => {
-            setValue(description, false);
-            clearSuggestions();
-            onAddressSelect && onAddressSelect(description);
-          }}
-        >
-          <strong>{main_text}</strong> <small>{secondary_text}</small>
-        </li>
-      );
-    });
-  };
-
-  return (
-    <div>
-      <input
-        value={value}
-        className={inputStyles}
-        disabled={!ready}
-        onChange={e => setValue(e.target.value)}
-        placeholder="Coordinates"
-      />
-
-      {status === 'OK' && <ul>{renderSuggestions()}</ul>}
     </div>
   );
 };
