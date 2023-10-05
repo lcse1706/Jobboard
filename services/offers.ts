eccfcb77096b5404ac70b733b218d5bddd6ae6ac
lukczar1706 <lcse.2001@gmail.com>
2023-10-05 20:40:04 +0200

    send to Airtable POST request

---
+++ services/offers.ts
@@ -0,0 +1,52 @@
+import { PlaceInfo } from '@/lib/types';
+
+interface OfferType {
+  title: string;
+  salary: string;
+  technologies: string;
+  location?: string;
+  coordinates?: any;
+  description: string;
+}
+
+interface AirTableFormatType {
+  fields: OfferType;
+}
+
+export const sendOffer = async (offer: OfferType, placeInfo: PlaceInfo) => {
+  const coord = { lat: placeInfo.lat, lng: placeInfo.lng };
+  // const coord = 'pewnie dlatego sie krzaczy';
+
+  const AirTableFormatOffer: AirTableFormatType = {
+    fields: {
+      title: offer.title,
+      salary: offer.salary,
+      technologies: offer.technologies,
+      location: placeInfo.placeName,
+      coordinates: JSON.stringify(coord),
+      description: offer.description,
+    },
+  };
+
+  const headers = {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_API_TOKEN}`,
+    'Content-Type': 'application/json',
+  };
+
+  const sendResponse: Response = await fetch(
+    `${process.env.NEXT_PUBLIC_AIRTABLE_BASE_URL}`,
+    {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(AirTableFormatOffer),
+    }
+  );
+
+  if (sendResponse.ok) {
+    console.log('Data send successful !');
+  } else {
+    throw new Error('Sending failed.');
+  }
+
+  return null;
+};
