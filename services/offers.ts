31b654e0688e97d2cbf4383714e5d6ec51a557a7
lukczar1706 <lcse.2001@gmail.com>
2023-10-09 20:55:00 +0200

    firebase connected instead of airtable

--- services/offers.ts
+++ services/offers.ts
@@ -1,50 +1,46 @@
 import { PlaceInfo } from '@/lib/types';
 
-interface OfferType {
-  logo: any;
+type OfferFirebaseType = {
+  // logo?: any;
   title: string;
   salary: string;
   technologies: string;
-  location?: string;
-  coordinates?: any;
+  location: string;
+  coordinates: any;
   description: string;
-}
+};
 
-interface AirTableFormatType {
-  fields: OfferType;
-}
+type OfferType = Omit<OfferFirebaseType, 'location' | 'coordinates'>;
 
 export const sendOffer = async (
-  offer: any,
-  placeInfo: PlaceInfo,
-  logo: any
+  offer: OfferType,
+  placeInfo: PlaceInfo
+  // logo: any
 ) => {
   const coord = { lat: placeInfo.lat, lng: placeInfo.lng };
   // const coord = 'pewnie dlatego sie krzaczy';
 
-  const AirTableFormatOffer: AirTableFormatType = {
-    fields: {
-      title: offer.title,
-      salary: offer.salary,
-      technologies: offer.technologies,
-      location: placeInfo.placeName,
-      coordinates: JSON.stringify(coord),
-      description: offer.description,
-      logo: logo,
-    },
+  const Offer: OfferFirebaseType = {
+    title: offer.title,
+    salary: offer.salary,
+    technologies: offer.technologies,
+    location: placeInfo.placeName,
+    coordinates: JSON.stringify(coord),
+    description: offer.description,
+    // logo: logo,
   };
 
   const headers = {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_API_TOKEN}`,
+    // Authorization: `Bearer ${process.env.NEXT_PUBLIC_AIRTABLE_API_TOKEN}`,
     'Content-Type': 'application/json',
   };
 
   const sendResponse: Response = await fetch(
-    `${process.env.NEXT_PUBLIC_AIRTABLE_BASE_URL}`,
+    `${process.env.NEXT_PUBLIC_FIREBASE_BASE_URL}`,
     {
       method: 'POST',
       headers: headers,
-      body: JSON.stringify(AirTableFormatOffer),
+      body: JSON.stringify(Offer),
     }
   );
 
