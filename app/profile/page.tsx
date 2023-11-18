21b079ff366761a0c1e1060b3055703e2cd1369e
lukczar1706 <lcse.2001@gmail.com>
2023-11-18 20:28:51 +0100

    profile page organized

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,10 +1,45 @@
 // Import your ProfileOffers component here
+import { getServerSession } from "next-auth";
+import Image from "next/image";
 
-const ProfilePage = () => {
+import { authConfig } from "@/lib/auth";
+
+import defaultLogo from "../favicon.ico";
+
+const ProfilePage = async () => {
+  const session = await getServerSession(authConfig);
+  console.log(session?.user?.image);
+  const logo = session?.user?.image;
   return (
     <div className="flex flex-col items-center justify-start h-screen">
-      <section className="ml-8">
-        <p>Profile Info</p>
+      <section className="flex flex-row mt-10">
+        {logo ? (
+          typeof logo === "string" ? (
+            <Image
+              src={logo}
+              alt="logo"
+              width={50}
+              height={50}
+              className="rounded-full mr-3"
+            />
+          ) : (
+            <Image
+              src={defaultLogo}
+              alt="default logo"
+              width={50}
+              height={50}
+              className="rounded-full mr-3"
+            />
+          )
+        ) : null}
+        <div>
+          <p>
+            <span className="font-bold">Name:</span> {session?.user?.name}
+          </p>
+          <p>
+            <span className="font-bold"> Email:</span> {session?.user?.email}
+          </p>
+        </div>
       </section>
     </div>
   );
