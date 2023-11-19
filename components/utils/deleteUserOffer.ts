d457863b5a32763342108bebc7143802356a6e22
lukczar1706 <lcse.2001@gmail.com>
2023-11-19 17:47:14 +0100

    name changed

---
+++ components/utils/deleteUserOffer.ts
@@ -0,0 +1,36 @@
+import { Session } from "next-auth";
+
+import { getUsers, updateUser } from "@/services";
+
+export const deleteUserOffer = async (
+  offerId: string,
+  session: Session | null | undefined,
+  pickOffers: string
+) => {
+  const users = await getUsers();
+
+  const sessionUser = session?.user?.email;
+
+  for (const user in users) {
+    if (sessionUser === users[user].email) {
+      for (let i = 0; i < users[user][pickOffers].length; i++) {
+        const offer = users[user][pickOffers][i];
+
+        if (offer === offerId) {
+          console.log("Delete offer with id: " + offerId);
+
+          // Remove offerId from array
+          const updatedArray = [...users[user][pickOffers]];
+          updatedArray.splice(i, 1);
+
+          const updatedUser = {
+            ...users[user],
+            [pickOffers]: updatedArray,
+          };
+
+          updateUser(user, updatedUser);
+        }
+      }
+    }
+  }
+};
