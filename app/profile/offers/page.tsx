060bc9069d043c7a5fabe26af498f04867de9cf1
lukczar1706 <lcse.2001@gmail.com>
2023-12-03 20:32:26 +0100

    update

--- app/profile/offers/page.tsx
+++ app/profile/offers/page.tsx
@@ -6,23 +6,22 @@ import { pickOffers } from "@/components/utils/pickOffers";
 import { ProfileOffers } from "./components/ProfileOffers";
 
 export default async function Offers() {
-  // const session = await getServerSession(authConfig);
+  const session = await getServerSession(authConfig);
 
-  // let userOffers = [];
-  // if (session) {
-  //   userOffers = await pickOffers("offersPublished");
-  // }
+  let userOffers = [];
+  if (session) {
+    userOffers = await pickOffers("offersPublished");
+  }
 
   return (
-    // <ul className="flex flex-col items-center justify-start dh-screen">
-    //   {userOffers.length > 0 ? (
-    //     userOffers.map((item) => <ProfileOffers key={item.id} data={item} />)
-    //   ) : (
-    //     <div className="bg-white p-8 rounded-lg border shadow-lg max-w-6xl mx-auto mt-8">
-    //       You have not published any offer yet !
-    //     </div>
-    //   )}
-    // </ul>
-    <div>My offers </div>
+    <ul className="flex flex-col items-center justify-start dh-screen">
+      {userOffers.length > 0 ? (
+        userOffers.map((item) => <ProfileOffers key={item.id} data={item} />)
+      ) : (
+        <div className="bg-white p-8 rounded-lg border shadow-lg max-w-6xl mx-auto mt-8">
+          You have not published any offer yet !
+        </div>
+      )}
+    </ul>
   );
 }
