0bfda84c09423eb156deca308eb5bc0152c66017
lukczar1706 <lcse.2001@gmail.com>
2023-12-13 21:11:18 +0100

    cleanup

--- components/ui/Navigation/Navigation.tsx
+++ components/ui/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
