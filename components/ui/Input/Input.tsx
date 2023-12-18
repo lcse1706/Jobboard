f61c87fc929c7f108b06aa84c1c090ef2deb99c3
lukczar1706 <lcse.2001@gmail.com>
2023-12-18 20:47:17 +0100

    refactor: feat packed in, classMerge added

--- components/ui/Input/Input.tsx
+++ components/ui/Input/Input.tsx
@@ -1,13 +1,36 @@
-import { ComponentProps } from "react";
-import { UseFormRegisterReturn } from "react-hook-form";
+import { ComponentPropsWithRef, ForwardedRef, forwardRef } from "react";
+import { FieldError } from "react-hook-form";
+
+import { classMerge } from "../utils/cn";
 
 type InputType = {
-  type: string;
-  register?: UseFormRegisterReturn;
-} & ComponentProps<"input">;
+  label: string;
+  error: FieldError;
+} & ComponentPropsWithRef<"input">;
 
-export const Input = ({ type, register, ...rest }: InputType) => {
-  return <input type={type} {...register} {...rest} />;
-};
+export const Input = forwardRef(
+  (
+    { type, className, placeholder, label, error, ...rest }: InputType,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    const id = Date.now().toString(20);
+
+    return (
+      <div>
+        <label className="hidden" htmlFor={id}>
+          {label}
+        </label>
+        <input
+          id={id}
+          ref={ref}
+          className={classMerge(className)}
+          placeholder={placeholder}
+          {...rest}
+        />
+        {error && <p className="text-red-500 mb-3">{error.message}</p>}
+      </div>
+    );
+  }
+);
 
 Input.displayName = "Input";
