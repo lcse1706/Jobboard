068b54216deebf6bf648dcf0e683381f0ca78611
lukczar1706 <lcse.2001@gmail.com>
2024-01-15 20:43:05 +0100

    cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,3 @@
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
@@ -36,7 +33,6 @@ export default function RootLayout({
           </Header>
           {children}
         </Providers>
-        <ToastContainer />
       </body>
     </html>
   );
