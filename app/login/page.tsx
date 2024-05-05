74eda71e1c396778cdac7546b59c59438de8e156
lukczar1706 <lcse.2001@gmail.com>
2024-05-05 13:27:40 +0200

    feat: mobile view

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,30 +2,28 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 import { GoogleSignInButton } from "@/components/ui/AuthButtons";
+import { isMobileDevice } from "@/lib/isMobileDevice";
 
 import { CredentialsForm } from "./components/CredentialsForm";
+import Login from "./components/Login";
+import Register from "./components/Register";
 import { RegisterForm } from "./components/RegisterForm";
 import { authConfig } from "./lib/auth";
 
 export default async function SignInPage() {
+  const isMobile = isMobileDevice();
+
   const session = await getServerSession(authConfig);
   if (session) return redirect("/");
 
   return (
-    <div className="w-full flex flex-row items-center justify-center min-dh-screen py-2">
-      <div className="flex flex-col items-center mt-10 p-10 mr-5 shadow-lg border border-gray-200 rounded-md bg-white">
-        <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
-        <GoogleSignInButton />
-
-        <span className="text-2xl font-semibold text-black text-center mt-8">
-          Or
-        </span>
-        <CredentialsForm />
-      </div>
-      <div className="flex flex-col items-center mt-10 p-10 shadow-lg border border-gray-200 rounded-md bg-white">
-        <h1 className="mt-10 mb-4 text-4xl font-bold">Register</h1>
-        <RegisterForm />
-      </div>
+    <div
+      className={
+        isMobile ? "flex flex-col" : "w-full flex flex-row justify-center"
+      }
+    >
+      <Login />
+      <Register />
     </div>
   );
 }
