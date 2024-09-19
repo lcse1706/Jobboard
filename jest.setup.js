f570a627f9f6af6289771eb774a660a887d912b6
lukczar1706 <lcse.2001@gmail.com>
2024-09-19 21:53:19 +0200

    feat: mock img

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
 import "@testing-library/jest-dom";
+
+jest.mock("next/image", () => ({
+  __esModule: true,
+  default: (props) => {
+    return <img {...props} />; // Mock as a regular `img` element
+  },
+}));
