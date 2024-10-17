782977103cf566e1f962b913649c0dd76400dcca
lukczar1706 <lcse.2001@gmail.com>
2024-10-17 15:03:46 +0200

    update: storybook packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,17 +8,16 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
-    "storybook-dark-mode",
-    {
-      name: "@storybook/addon-styling",
-      options: {
-        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
-        // For more details on this addon's options.
-        postCss: {
-          implementation: require.resolve("postcss"),
-        },
-      },
-    },
+    // "@storybook/addon-styling-webpack",
+    "@storybook/addon-themes",
+    // {
+    //   name: "@storybook/addon-styling",
+    //   options: {
+    //     postCss: {
+    //       implementation: require.resolve("postcss"),
+    //     },
+    //   },
+    // },
   ],
   framework: {
     name: "@storybook/nextjs",
