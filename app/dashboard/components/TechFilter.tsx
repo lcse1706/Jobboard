eb9f9dc6479a49d45a514567a2803e1c61d0548b
lukczar1706 <lcse.2001@gmail.com>
2024-10-31 11:07:19 +0100

    feat(filter): add two-level tech filtering with FontAwesome icons
    - Implement tech filtering with FontAwesome icons for multiple technologies (JavaScript, React, Vue, Node.js, etc.)
    - Use `activeTech` state to manage selected tech and highlight active filter
    - Update `setTechFilteredData` context dynamically based on selected technology in `activeTech`
    - Ensure icons visually indicate active filters with a color change

--- app/dashboard/components/TechFilter.tsx
+++ app/dashboard/components/TechFilter.tsx
@@ -27,129 +27,143 @@ import "./TechFilter.css";
 
 export const TechFilter = () => {
   const { records, setTechFilteredData } = useDataContext();
-  const [searchField, setSearchField] = useState("");
+  const [activeTech, setActiveTech] = useState("");
 
   useEffect(() => {
     const filteredTech = records.filter((record) => {
       return (
-        record.title.toLowerCase().includes(searchField) ||
-        record.technologies.toLowerCase().includes(searchField) ||
-        record.description.toLowerCase().includes(searchField)
+        record.title.toLowerCase().includes(activeTech) ||
+        record.technologies.toLowerCase().includes(activeTech) ||
+        record.description.toLowerCase().includes(activeTech)
       );
     });
     setTechFilteredData(filteredTech);
-    console.log(searchField);
-  }, [searchField, records]);
+  }, [activeTech, records]);
+
+  const filterHandler = (tech: string) => {
+    setActiveTech((prevTech) => (prevTech === tech ? "" : tech));
+  };
 
   return (
-    <div className="custom-scrollbar text-6xl mx-7  bg-gray-200 text-black dark:bg-inherit dark:text-white [&>*]:p-4 overflow-x-auto whitespace-nowrap">
+    <div className="custom-scrollbar text-6xl mx-7  bg-gray-200 text-black dark:bg-inherit dark:text-white [&>*]:m-4 [&>*]:cursor-pointer overflow-x-auto whitespace-nowrap">
+      <FontAwesomeIcon
+        icon={faJs}
+        onClick={() => {
+          filterHandler("javascript");
+        }}
+        className={`${activeTech === "javascript" ? "text-blue-500" : ""}`}
+      />
       <FontAwesomeIcon
         icon={faReact}
-        onClick={() => {
-          setSearchField("react");
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faPython}
-        onClick={() => {
-          setSearchField("python");
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faWordpress}
-        onClick={() => {
-          setSearchField("wordpress");
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faDocker}
-        onClick={() => {
-          setSearchField("docker");
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faHtml5}
-        onClick={() => {
-          setSearchField("html");
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faLinux}
-        onClick={() => {
-          setSearchField("linux");
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faJava}
-        onClick={() => {
-          setSearchField("java");
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faNodeJs}
-        onClick={() => {
-          setSearchField("node");
-        }}
+        onClick={() => filterHandler("react")}
+        className={`${activeTech === "react" ? "text-blue-500" : ""}`}
       />
       <FontAwesomeIcon
         icon={faVuejs}
         onClick={() => {
-          setSearchField("vue");
+          filterHandler("vue");
         }}
+        className={`${activeTech === "vue" ? "text-blue-500" : ""}`}
       />
       <FontAwesomeIcon
-        icon={faSymfony}
+        icon={faNodeJs}
         onClick={() => {
-          setSearchField("symfony");
+          filterHandler("node");
         }}
+        className={`${activeTech === "node" ? "text-blue-500" : ""}`}
       />
       <FontAwesomeIcon
-        icon={faRust}
+        icon={faPython}
         onClick={() => {
-          setSearchField("rust");
+          filterHandler("python");
         }}
+        className={`${activeTech === "python" ? "text-blue-500" : ""}`}
       />
       <FontAwesomeIcon
-        icon={faPhp}
+        icon={faWordpress}
         onClick={() => {
-          setSearchField("php");
+          filterHandler("wordpress");
         }}
+        className={`${activeTech === "wordpress" ? "text-blue-500" : ""}`}
       />
       <FontAwesomeIcon
-        icon={faLaravel}
+        icon={faDocker}
         onClick={() => {
-          setSearchField("laravel");
+          filterHandler("docker");
         }}
+        className={`${activeTech === "docker" ? "text-blue-500" : ""}`}
       />
       <FontAwesomeIcon
-        icon={faJs}
+        icon={faHtml5}
         onClick={() => {
-          setSearchField("javascript");
+          filterHandler("html");
         }}
-      />
-      <FontAwesomeIcon
-        icon={faGolang}
-        onClick={() => {
-          setSearchField("golang");
-        }}
-      />
-      <FontAwesomeIcon
-        icon={faRust}
-        onClick={() => {
-          setSearchField("rust");
-        }}
+        className={`${activeTech === "html" ? "text-blue-500" : ""}`}
       />
       <FontAwesomeIcon
         icon={faCss3Alt}
         onClick={() => {
-          setSearchField("css");
+          filterHandler("css");
         }}
+        className={`${activeTech === "css" ? "text-blue-500" : ""}`}
       />
+      <FontAwesomeIcon
+        icon={faLinux}
+        onClick={() => {
+          filterHandler("linux");
+        }}
+        className={`${activeTech === "linux" ? "text-blue-500" : ""}`}
+      />
+      <FontAwesomeIcon
+        icon={faJava}
+        onClick={() => {
+          filterHandler("java");
+        }}
+        className={`${activeTech === "java" ? "text-blue-500" : ""}`}
+      />
+      <FontAwesomeIcon
+        icon={faSymfony}
+        onClick={() => {
+          filterHandler("symfony");
+        }}
+        className={`${activeTech === "symfony" ? "text-blue-500" : ""}`}
+      />
+
+      <FontAwesomeIcon
+        icon={faPhp}
+        onClick={() => {
+          filterHandler("php");
+        }}
+        className={`${activeTech === "php" ? "text-blue-500" : ""}`}
+      />
+      <FontAwesomeIcon
+        icon={faLaravel}
+        onClick={() => {
+          filterHandler("laravel");
+        }}
+        className={`${activeTech === "laravel" ? "text-blue-500" : ""}`}
+      />
+      <FontAwesomeIcon
+        icon={faGolang}
+        onClick={() => {
+          filterHandler("golang");
+        }}
+        className={`${activeTech === "golang" ? "text-blue-500" : ""}`}
+      />
+      <FontAwesomeIcon
+        icon={faRust}
+        onClick={() => {
+          filterHandler("rust");
+        }}
+        className={`${activeTech === "rust" ? "text-blue-500" : ""}`}
+      />
+
       <FontAwesomeIcon
         icon={faAws}
         onClick={() => {
-          setSearchField("aws");
+          filterHandler("aws");
         }}
+        className={`${activeTech === "aws" ? "text-blue-500" : ""}`}
       />
     </div>
   );
