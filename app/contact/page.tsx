9a69504a7b8c37099c9a1ab60ad1c7f8ea733a08
lukczar1706 <lcse.2001@gmail.com>
2024-11-08 13:42:12 +0100

    cleanup

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <div className="flex justify-center w-screen ">
-      <h1 className="mt-10">Working in progress</h1>
+      <h1 className="mt-10">Work in progress</h1>
     </div>
   );
 };
